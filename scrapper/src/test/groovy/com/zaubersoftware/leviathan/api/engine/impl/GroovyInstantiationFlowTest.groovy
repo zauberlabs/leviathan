3a8a11ae212595db391904166f8c1378ce1fffa5
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2011-12-29 21:19:58 -0300

    Simplifying Syntax

--- scrapper/src/test/groovy/com/zaubersoftware/leviathan/api/engine/impl/GroovyInstantiationFlowTest.groovy
+++ scrapper/src/test/groovy/com/zaubersoftware/leviathan/api/engine/impl/GroovyInstantiationFlowTest.groovy
@@ -52,162 +52,162 @@ import com.zaubersoftware.leviathan.api.engine.impl.dto.Link;
  * @author Martin Silva
  * @since Sep 2, 2011
  */
-public final class GroovyInstantiationFlowTest {
+final class GroovyInstantiationFlowTest {
 
-    private final URI mlhome = URI.create("http://www.mercadolibre.com.ar/");
-    private AsyncUriFetcher fetcher;
-    private Engine engine;
-    private URIFetcher f;
+    final URI mlhome = URI.create("http://www.mercadolibre.com.ar/")
+    AsyncUriFetcher fetcher
+    Engine engine
+    URIFetcher f
 
     @Before
-    public void setUp() {
-        final Map<URI, String> pages = new HashMap<URI, String>();
+    void setUp() {
+        final Map<URI, String> pages = new HashMap<URI, String>()
         f = Fetchers.createFixed().register(mlhome, 
-                "com/zaubersoftware/leviathan/api/engine/pages/homeml.html").build();
-        final ExecutorService executor = Executors.newSingleThreadExecutor();
-        this.fetcher = new ExecutorServiceAsyncUriFetcher(executor);
+                "com/zaubersoftware/leviathan/api/engine/pages/homeml.html").build()
+        final ExecutorService executor = Executors.newSingleThreadExecutor()
+        this.fetcher = new ExecutorServiceAsyncUriFetcher(executor)
 
-        this.engine = Leviathan.flowBuilder();
+        this.engine = Leviathan.flowBuilder()
     }
 
 
 
 
     @Test
-    public void shouldFetchAndDoSomethingWithAClosure() throws Exception {
-        final AtomicBoolean fetchPerformed = new AtomicBoolean(false);
+    void shouldFetchAndDoSomethingWithAClosure() throws Exception {
+        final AtomicBoolean fetchPerformed = new AtomicBoolean(false)
         final ProcessingFlow flow = this.engine.afterFetch().then(new ContextAwareClosure<URIFetcherResponse>() {
             @Override
-            public void execute(final URIFetcherResponse response) {
-                assertTrue(response.isSucceeded());
-                fetchPerformed.set(true);
+            void execute(final URIFetcherResponse response) {
+                assertTrue(response.isSucceeded())
+                fetchPerformed.set(true)
             }
-        }).pack();
+        }).pack()
 
-        this.fetcher.scheduleFetch(f.createGet(this.mlhome), flow).awaitIdleness();
-        assertTrue("Did not fetch!", fetchPerformed.get());
+        this.fetcher.scheduleFetch(f.createGet(this.mlhome), flow).awaitIdleness()
+        assertTrue("Did not fetch!", fetchPerformed.get())
 
     }
 
     @Test
-    public void shouldFetchDoSomethingAndHandleTheExceptionWithoutConfiguredHandlers() throws Exception {
-        final AtomicBoolean exceptionHandled = new AtomicBoolean(false);
+    void shouldFetchDoSomethingAndHandleTheExceptionWithoutConfiguredHandlers() throws Exception {
+        final AtomicBoolean exceptionHandled = new AtomicBoolean(false)
         final RuntimeException exception = 
-                new MockException("an exception was thrown while processing the response!");
+                new MockException("an exception was thrown while processing the response!")
         ProcessingFlow flow = this.engine.afterFetch().then(new ContextAwareClosure<URIFetcherResponse>() {
             @Override
-            public void execute(final URIFetcherResponse response) {
-                throw exception;
+            void execute(final URIFetcherResponse response) {
+                throw exception
             }
         }).onAnyExceptionDo(new ExceptionHandler() {
             @Override
-            public void handle(final Throwable trowable) {
-                exceptionHandled.set(true);
-                assertEquals(exception, trowable);
+            void handle(final Throwable trowable) {
+                exceptionHandled.set(true)
+                assertEquals(exception, trowable)
             }
-        }).pack();
-        fetcher.scheduleFetch(f.createGet(mlhome), flow).awaitIdleness();
-        assertTrue("Did not hadle the exception", exceptionHandled.get());
+        }).pack()
+        fetcher.scheduleFetch(f.createGet(mlhome), flow).awaitIdleness()
+        assertTrue("Did not hadle the exception", exceptionHandled.get())
     }
 
     @Test
-    public void shouldFetchDoSomethingAndHandleTheExceptionWithAnSpecificHandler() throws Exception {
-        final AtomicBoolean exceptionHandled = new AtomicBoolean(false);
+    void shouldFetchDoSomethingAndHandleTheExceptionWithAnSpecificHandler() throws Exception {
+        final AtomicBoolean exceptionHandled = new AtomicBoolean(false)
         final RuntimeException exception = 
-                new MockException("an exception was thrown while processing the response!");
+                new MockException("an exception was thrown while processing the response!")
         ProcessingFlow pack = this.engine.afterFetch().then(new ContextAwareClosure<URIFetcherResponse>() {
             @Override
-            public void execute(final URIFetcherResponse response) {
-                throw exception;
+            void execute(final URIFetcherResponse response) {
+                throw exception
             }
         }).on(MockException.class).handleWith(new ExceptionHandler() {
             @Override
-            public void handle(final Throwable trowable) {
-                exceptionHandled.set(true);
-                assertEquals(exception, trowable);
+            void handle(final Throwable trowable) {
+                exceptionHandled.set(true)
+                assertEquals(exception, trowable)
             }
         }).otherwiseHandleWith(new ExceptionHandler() {
             @Override
-            public void handle(final Throwable trowable) {
+            void handle(final Throwable trowable) {
                 fail("It should never reach here, the exception should be handled by the configured handler."
-                        + " Look above!!!");
+                        + " Look above!!!")
             }
-        }).pack();
-        this.fetcher.scheduleFetch(f.createGet(mlhome), pack).awaitIdleness();
-        assertTrue("Did not hadle the exception", exceptionHandled.get());
+        }).pack()
+        this.fetcher.scheduleFetch(f.createGet(mlhome), pack).awaitIdleness()
+        assertTrue("Did not hadle the exception", exceptionHandled.get())
     }
 
     @Test
-    public void shouldBindUriToAFlow() throws Exception {
-        final AtomicBoolean fetchPerformed = new AtomicBoolean(false);
+    void shouldBindUriToAFlow() throws Exception {
+        final AtomicBoolean fetchPerformed = new AtomicBoolean(false)
         final ProcessingFlow flow = this.engine
             .afterFetch()
             .then(new ContextAwareClosure<URIFetcherResponse>() {
                 @Override
-                public void execute(final URIFetcherResponse response) {
-                    assertTrue(response.isSucceeded());
-                    fetchPerformed.set(true);
+                void execute(final URIFetcherResponse response) {
+                    assertTrue(response.isSucceeded())
+                    fetchPerformed.set(true)
                 }
             })
-            .pack();
+            .pack()
 
-        fetcher.scheduleFetch(f.createGet(mlhome), flow).awaitIdleness();
-        assertTrue("Did not fetch!", fetchPerformed.get());
+        fetcher.scheduleFetch(f.createGet(mlhome), flow).awaitIdleness()
+        assertTrue("Did not fetch!", fetchPerformed.get())
     }
 
     @Test
-    public void shouldHaveContext() throws Exception {
-        final String key = "FOO";
-        final String val = "VAL";
+    void shouldHaveContext() throws Exception {
+        final String key = "FOO"
+        final String val = "VAL"
 
-        final AtomicBoolean fetchPerformed = new AtomicBoolean(false);
+        final AtomicBoolean fetchPerformed = new AtomicBoolean(false)
         final ProcessingFlow flow = this.engine
             .afterFetch()
             .then(new ContextAwareClosure<URIFetcherResponse>() {
                 @Override
-                public void execute(final URIFetcherResponse response) {
-                    assertTrue(response.isSucceeded());
-                    assertEquals(val, get(key));
-                    fetchPerformed.set(true);
+                void execute(final URIFetcherResponse response) {
+                    assertTrue(response.isSucceeded())
+                    assertEquals(val, get(key))
+                    fetchPerformed.set(true)
                 }
             })
-            .pack();
+            .pack()
 
-        final Map<String, Object> ctx = new HashMap<String, Object>();
-        ctx.put(key, val);
-        fetcher.scheduleFetch(f.createGet(new InmutableURIAndCtx(this.mlhome, ctx)), flow).awaitIdleness();
-        assertTrue("Did not fetch!", fetchPerformed.get());
+        final Map<String, Object> ctx = new HashMap<String, Object>()
+        ctx.put(key, val)
+        fetcher.scheduleFetch(f.createGet(new InmutableURIAndCtx(this.mlhome, ctx)), flow).awaitIdleness()
+        assertTrue("Did not fetch!", fetchPerformed.get())
     }
 
     @Test
-    public void shouldHaveContextAndCanBeShareBetweenActions() throws Exception {
-        final String KEY = "FOO";
-        final String VAL = "VAL";
+    void shouldHaveContextAndCanBeShareBetweenActions() throws Exception {
+        final String KEY = "FOO"
+        final String VAL = "VAL"
 
-        final AtomicBoolean fetchPerformed = new AtomicBoolean(false);
+        final AtomicBoolean fetchPerformed = new AtomicBoolean(false)
         final ProcessingFlow flow = this.engine
             .afterFetch()
             .then(new ContextAwareClosure<URIFetcherResponse>() {
                 @Override
-                public void execute(final URIFetcherResponse response) {
-                    assertTrue(response.isSucceeded());
-                    assertEquals(VAL, get(KEY));
-                    fetchPerformed.set(true);
+                void execute(final URIFetcherResponse response) {
+                    assertTrue(response.isSucceeded())
+                    assertEquals(VAL, get(KEY))
+                    fetchPerformed.set(true)
                 }
             })
-            .pack();
+            .pack()
 
-        final Map<String, Object> ctx = new HashMap<String, Object>();
-        ctx.put(KEY, VAL);
-        fetcher.scheduleFetch(f.createGet(new InmutableURIAndCtx(this.mlhome, ctx)), flow).awaitIdleness();
-        assertTrue("Did not fetch!", fetchPerformed.get());
+        final Map<String, Object> ctx = new HashMap<String, Object>()
+        ctx.put(KEY, VAL)
+        fetcher.scheduleFetch(f.createGet(new InmutableURIAndCtx(this.mlhome, ctx)), flow).awaitIdleness()
+        assertTrue("Did not fetch!", fetchPerformed.get())
     }
 
     @Test
-    public void shouldFlow() throws Exception {
+    void shouldFlow() throws Exception {
         final Source xsltSource = new StreamSource(getClass().getClassLoader().getResourceAsStream(
-        "com/zaubersoftware/leviathan/api/engine/stylesheet/html.xsl"));
-        final AtomicBoolean actionPerformed = new AtomicBoolean(false);
+        "com/zaubersoftware/leviathan/api/engine/stylesheet/html.xsl"))
+        final AtomicBoolean actionPerformed = new AtomicBoolean(false)
         final ProcessingFlow pack = this.engine
             .afterFetch()
             .sanitizeHTML()
@@ -215,55 +215,55 @@ public final class GroovyInstantiationFlowTest {
             .toJavaObject(Link.class)
             .then(new Action<Link, String>() {
                 @Override
-                public String execute(final Link link) {
-                    actionPerformed.set(true);
-                    return link.getTitle();
+                String execute(final Link link) {
+                    actionPerformed.set(true)
+                    return link.getTitle()
                 }
             })
             .then(new ContextAwareClosure<String>() {
                 @Override
-                public void execute(final String input) {
-                    assertEquals("MercadoLibre Argentina - Donde comprar y vender de todo.", input);
+                void execute(final String input) {
+                    assertEquals("MercadoLibre Argentina - Donde comprar y vender de todo.", input)
                 }
             })
-            .pack();
-        fetcher.scheduleFetch(f.createGet(mlhome), pack).awaitIdleness();
-        assertTrue("Did not hadle the exception", actionPerformed.get());
+            .pack()
+        fetcher.scheduleFetch(f.createGet(mlhome), pack).awaitIdleness()
+        assertTrue("Did not hadle the exception", actionPerformed.get())
     }
 
     @Test
-    public void shouldForEachFlow() throws Exception {
+    void shouldForEachFlow() throws Exception {
         final Source xsltSource = new StreamSource(getClass().getClassLoader().getResourceAsStream(
-        "com/zaubersoftware/leviathan/api/engine/stylesheet/html.xsl"));
-        final AtomicBoolean actionPerformed = new AtomicBoolean(false);
-        final AtomicInteger count = new AtomicInteger(0);
+        "com/zaubersoftware/leviathan/api/engine/stylesheet/html.xsl"))
+        final AtomicBoolean actionPerformed = new AtomicBoolean(false)
+        final AtomicInteger count = new AtomicInteger(0)
         final ProcessingFlow pack = this.engine.afterFetch()
             .sanitizeHTML()
             .transformXML(xsltSource)
             .toJavaObject(Link.class)
             .then(new Action<Link, Link>() {
                 @Override
-                public Link execute(final Link link) {
-                    actionPerformed.set(true);
-                    return link;
+                Link execute(final Link link) {
+                    actionPerformed.set(true)
+                    return link
                 }
             })
             .forEach(String.class).in("categories")
                 .then(new ContextAwareClosure<String>() {
                     @Override
-                    public void execute(final String category) {
-                        count.incrementAndGet();
+                    void execute(final String category) {
+                        count.incrementAndGet()
                     }
                 })
             .endFor()
             .then(new ContextAwareClosure<Link>() {
                 @Override
-                public void execute(final Link link) {
-                    assertEquals(4, count.get());
+                void execute(final Link link) {
+                    assertEquals(4, count.get())
                 }
             })
-            .pack();
-        fetcher.scheduleFetch(f.createGet(mlhome), pack).awaitIdleness();
-        assertTrue("Did not hadle the exception", actionPerformed.get());
+            .pack()
+        fetcher.scheduleFetch(f.createGet(mlhome), pack).awaitIdleness()
+        assertTrue("Did not hadle the exception", actionPerformed.get())
     }
 }
