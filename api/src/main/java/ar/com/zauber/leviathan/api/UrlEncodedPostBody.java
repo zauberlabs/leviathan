7080212b3b79c1ac2ef4b003049ae097be5ab19b
Juan F. Codagnone <juan@zauberlabs.com>
2012-04-18 23:35:21 -0300

    actualizo el header de licencia al 2012. aprovecho a cambiar a zauberlabs.com

--- api/src/main/java/ar/com/zauber/leviathan/api/UrlEncodedPostBody.java
+++ api/src/main/java/ar/com/zauber/leviathan/api/UrlEncodedPostBody.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009-2011 Zauber S.A. <http://www.zaubersoftware.com/>
+ * Copyright (c) 2009-2012 Zauber S.A. <http://zauberlabs.com/>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * Copyright (c) 2009-2010 Zauber S.A. <http://www.zaubersoftware.com/>
  *
